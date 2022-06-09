af772665703228a7f0d0a0d162ada8d828ceddc1
JMBuccolini <96688216+JMBuccolini@users.noreply.github.com>
2022-06-08 22:26:52 -0300

    Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+function Navbar() {
+  return (
+  
+    <nav className="navbar">
+     
+        <span className="navbar-brand">
+            <a href="/home">HENRY DOGS</a>
+        </span>
+        <div className="column">
+
+        </div>
+        <div className="column">
+            
+        </div>
+    </nav>
+
+  );
+};
+
+export default Navbar;
